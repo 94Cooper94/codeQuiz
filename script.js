bcd1c5084bc22b1b1db6a02afc4bd31e8cb4c2c8
Cooper <eliteswimmer42@gmail.com>
2020-08-07 17:22:52 -0500

    Created a template timer.

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// START TIMER CODE
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 60;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+function sendMessage() {
+  timeEl.textContent = " ";
+
+  var imgEl = document.createElement("img");
+
+  imgEl.setAttribute("src", "media/stop-hand.jpg");
+  mainEl.appendChild(imgEl);
+}
+setTime();
+// END TIMER CODE
