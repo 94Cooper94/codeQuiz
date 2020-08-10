c7febe0505c2fc8f6230f7693d48e1a5ec9751e7
Cooper <eliteswimmer42@gmail.com>
2020-08-09 21:26:22 -0500

    Trying to generate document elements. Populated the index with a scoretracker element, start button, and then a button group with four options to placehold the quiz content.

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-document.querySelector("#generate").addEventListener("click",setTime);
 // START TIMER CODE
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
 
-var secondsLeft = 60;
+// var timeEl = document.querySelector(".time");
+// var mainEl = document.getElementById("main");
 
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = "Time: " + secondsLeft;
+// var secondsLeft = 60;
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      sendMessage();
-    }
-  }, 1000);
-}
-function sendMessage() {
-  timeEl.textContent = " ";
+// function setTime() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = "Time: " + secondsLeft;
 
-  var imgEl = document.createElement("img");
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       sendMessage();
+//     }
+//   }, 1000);
+// }
+// function sendMessage() {
+//   timeEl.textContent = " ";
 
-  imgEl.setAttribute("src", "media/stop-hand.jpg");
-  mainEl.appendChild(imgEl);
-}
-setTime();
+//   var imgEl = document.createElement("img");
+
+//   imgEl.setAttribute("src", "media/stop-hand.jpg");
+//   mainEl.appendChild(imgEl);
+// }
+// setTime();
+
 // END TIMER CODE
+
+
+// SCORE TRACKER
+
+var counter = document.querySelector("#counter");
+
+var count = localStorage.getItem("count");
+
+counter.textContent = count;
+
+addButton.addEventListener("click", function() {
+  count++;
+  counter.textContent = count;
+
+  localStorage.setItem("count", count);
+});
+
+subtractButton.addEventListener("click", function() {
+  count--;
+  counter.textContent = count;
+
+  localStorage.setItem("count", count);
+});
+
+// END SCORE TRACKER
